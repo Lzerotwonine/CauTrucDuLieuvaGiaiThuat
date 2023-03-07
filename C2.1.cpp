4e0aa5a56a5fb56e1e8ccda9445f042d1f5df36d
Lzerotwonine <125208234+Lzerotwonine@users.noreply.github.com>
2023-03-08 00:44:18 +0700

    Add files via upload

---
+++ C2.1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int n, i, s;
+	
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	
+	s = 0;
+	for (i = 1; i <= n; i++)
+		s = s + i;
+	
+	printf("1 + 2 + ... + %d = %d", n, s);
+}
