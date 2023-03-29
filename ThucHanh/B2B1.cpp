1ca99a85629ec1b771c2486525191585b3779f1f
Lzerotwonine <125208234+Lzerotwonine@users.noreply.github.com>
2023-03-29 16:21:03 +0700

    Update B2B1.cpp

--- ThucHanh/B2B1.cpp
+++ ThucHanh/B2B1.cpp
@@ -3,7 +3,7 @@
 // B2Buoi1. Viết hàm đệ quy tính tổng của biểu thức sau đây: S(x, n) = x + x^2/2! + x^3/3! + … + x^n/n!
 
 float Tong(float x, int n) {
-	if(n == 0)
+    if(n == 0)
         return 0;
     if(n == 1)
         return x;
@@ -11,20 +11,20 @@ float Tong(float x, int n) {
 }
 
 int main() {
-	int n;
-	float x, T;
+    int n;
+    float x, T;
 	
-	do {
-		printf("Nhap n: ");
-		scanf("%d", &n);
-	} while(n < 0);
+    do {
+        printf("Nhap n: ");
+        scanf("%d", &n);
+    } while(n < 0);
 	
-	do {
-		printf("Nhap x: ");
-		scanf("%f", &x);
-	} while(x < 0);
+    do {
+        printf("Nhap x: ");
+        scanf("%f", &x);
+    } while(x < 0);
 	
-	T = Tong(x, n);
-	printf("Tong = %.2f", T);
-	return 0;
+    T = Tong(x, n);
+    printf("Tong = %.2f", T);
+    return 0;
 }
